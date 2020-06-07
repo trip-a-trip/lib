c0d580c74e2100aae315344c16e531f4b9aea498
Igor Kamyshev <igor@kamyshev.me>
2020-06-07 08:13:59 +0700

    feat(shared): add bull constants

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { UserClient } from './core-services/UserClient';
 export { EatClient } from './core-services/EatClient';
 
 export * from './core-services/EatClient.types';
+export * from './shared/bull.constants';
